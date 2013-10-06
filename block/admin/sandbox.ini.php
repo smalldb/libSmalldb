324e01517fe4d5157a963f228b9cc6f6ffb989db
Josef Kufner <jk@frozen-doe.net>
2013-10-06 23:44:01 +0200

    Sandbox cleanup

--- block/admin/sandbox.ini.php
+++ block/admin/sandbox.ini.php
@@ -4,7 +4,7 @@
 [block:print_r]
 .block = "core/out/print_r"
 .x = 0
-.y = 537
+.y = 358
 data[] = "config:entities"
 title = "entities"
 slot_weight = "60"
@@ -12,21 +12,21 @@ slot_weight = "60"
 [block:show_diagram_article]
 .block = "smalldb/show_diagram"
 .x = 171
-.y = 351
+.y = 172
 machine_type = "article"
 slot_weight = "45"
 
 [block:show_diagram_item]
 .block = "smalldb/show_diagram"
 .x = 162
-.y = 179
+.y = 0
 machine_type = "item"
 slot_weight = "35"
 
 [block:header_item]
 .block = "core/out/header"
 .x = 1
-.y = 180
+.y = 1
 level = "2"
 text = "Item"
 slot_weight = "30"
@@ -34,7 +34,7 @@ slot_weight = "30"
 [block:header_article]
 .block = "core/out/header"
 .x = 1
-.y = 342
+.y = 163
 level = "2"
 text = "Article"
 slot_weight = "40"
@@ -42,32 +42,17 @@ slot_weight = "40"
 [block:create]
 .block = "item/create"
 .x = 376
-.y = 214
+.y = 35
 
 [block:delete]
 .block = "item/delete"
 .x = 377
-.y = 386
+.y = 207
 
 [block:edit]
 .block = "item/edit"
 .x = 376
-.y = 293
-
-[block:show_diagram_auction]
-.block = "smalldb/show_diagram"
-.x = 162
-.y = 20
-machine_type = "auction_item"
-slot_weight = "25"
-
-[block:header_auction]
-.block = "core/out/header"
-.x = 3
-.y = 0
-level = 2
-text = "Auction Item"
-slot_weight = 20
+.y = 114
 
 
 ; vim:filetype=dosini:
