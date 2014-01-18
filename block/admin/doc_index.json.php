dc3dc1689f7d1d0546d16a331146af3c2a777fcd
Josef Kufner <jk@frozen-doe.net>
2014-01-18 01:14:05 +0100

    Convert *.ini.php to *.json.php

---
+++ block/admin/doc_index.json.php
@@ -0,0 +1,28 @@
+{
+    "_": "<?php printf('_%c%c}%c',34,10,10);__halt_compiler();?>",
+    "block:header_item": {
+        ".block": "core/out/header",
+        ".x": 0,
+        ".y": 3,
+        "level": 2,
+        "text": "Smalldb machines",
+        "slot_weight": 30
+    },
+    "block:build_types_menu": {
+        ".block": "smalldb/build_types_menu",
+        ".x": 170,
+        ".y": 0
+    },
+    "block:menu": {
+        ".block": "core/out/menu",
+        ".x": 427,
+        ".y": 0,
+        "enable": [
+            "build_types_menu:done"
+        ],
+        "items": [
+            "build_types_menu:items"
+        ],
+        "title_fmt": "{a}{type}{/a}"
+    }
+}
