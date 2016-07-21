606a1c0cf62761c3119ebb579b3ec3fa93be73cd
Josef Kufner <josef@kufner.cz>
2016-07-21 21:17:09 +0200

    Basic ligin & logout implementation - CookieAuth & SharedTokenMachine
    Tokens do not expire yet and database schema is hardwired.

---
+++ class/Auth/CookieAuth.php
@@ -0,0 +1,206 @@
+<?php
+/*
+ * Copyright (c) 2014-2016, Josef Kufner  <josef@kufner.cz>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+namespace Smalldb\StateMachine\Auth;
+use Smalldb\StateMachine;
+
+/**
+ * IAuth implementation using %Smalldb state machine and shared session token 
+ * in a cookie to identify SharedTokenMachine instance.
+ *
+ * This class handles cookies only, it is up to session machine to maintain session.
+ *
+ * To log in, use Auth::getSessionMachine() and invoke proper transition.
+ *
+ * To log out, use only the defined logout transition, otherwise token will not
+ * get deleted.
+ *
+ * Configuration options:
+ *   - `smalldb`: Instance of Smalldb\StateMachine\AbstractBackend (mandatory).
+ *   - `machine_ref_prefix`: Prefix of machine ID (array; mandatory).
+ *   - `machine_null_ref`: State machine type for null ref.
+ *   - `cookie_name`: Name of the cookie for a token. (default: 'auth')
+ *   - `cookie_ttl`: How long cookie is valid. (default: 10 years)
+ *   - `session_ttl`: Duration of the session (should be greater or equal to `cookie_ttl`, default: 10 years).
+ *   - `user_id_property`: Name of the session machine property with user's ID.
+ *   - `user_role_property`: Name of the session machine property with user's role. You may want to have this property calculated.
+ */
+class CookieAuth implements \Smalldb\StateMachine\Auth\IAuth
+{
+	protected $smalldb;					///< Smalldb backend
+	protected $session_machine;				///< Reference to session state machine
+
+	// Configuration
+	protected $cookie_name = 'AuthToken';			///< Cookie name
+	protected $cookie_ttl = 2592000;			///< Cookie duration [seconds] (default: 30 days)
+	protected $session_ttl = 2592000;			///< Session duration [seconds] (default: 30 days)
+	protected $user_id_property = 'user_id';		///< Name of the session machine property with user's ID
+	protected $user_roles_property = 'user_roles';		///< Name of the session machine property with user's role
+	protected $all_mighty_user_role = null;			///< Name of all mighty user role (admin)
+	protected $all_mighty_cli = false;			///< Is command line all mighty?
+	protected $session_machine_null_ref = 'session';	///< Null reference to session machine (array; use session_machine_ref_prefix if not set)
+	protected $session_machine_ref_prefix = 'session';	///< Prefix of session machine reference (array; token ID will be appended)
+
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct($config, \Smalldb\StateMachine\AbstractBackend $smalldb)
+	{
+		$this->smalldb = $smalldb;
+
+		// Load configuration
+		foreach (array('cookie_name', 'cookie_ttl', 'session_ttl',
+			'user_id_property', 'all_mighty_user_role', 'all_mighty_cli',
+			'session_machine_null_ref', 'session_machine_ref_prefix') as $p)
+		{
+			if (isset($config[$p])) {
+				$this->$p = $config[$p];
+			}
+		}
+
+		// Check session machine name
+		if (empty($this->session_machine_ref_prefix)) {
+			throw new \InvalidArgumentException('Session machine not specified (see SharedTokenMachine).');
+		}
+		if (empty($this->session_machine_null_ref)) {
+			$this->session_machine_null_ref = $this->session_machine_ref_prefix;
+		}
+
+		$this->session_machine = null;
+	}
+
+
+	/**
+	 * Check session - read & update cookies, setup session state machine and register callbacks.
+	 *
+	 * This must be called before using any state machines. No transitions
+	 * are invoked at this point, only the session state machine reference
+	 * is created.
+	 */
+	public function checkSession()
+	{
+		// Get session token
+		$session_token = @ $_COOKIE[$this->cookie_name];
+
+		// Get reference to session state machine
+		if ($session_token) {
+			// Get session machine ref
+			$session_id = (array) $this->session_machine_ref_prefix;
+			$session_id[] = $session_token;
+			$this->session_machine = $this->smalldb->ref($session_id);
+
+			// Check session
+			if ($this->session_machine->state == '') {
+				// Invalid token
+				$this->session_machine = $this->smalldb->ref($this->session_machine_null_ref);
+			} else {
+				// Token is good - refresh cookie
+				setcookie($this->cookie_name, $session_token, time() + $this->cookie_ttl, '/', null, !empty($_SERVER['HTTPS']), true);
+			}
+		} else {
+			// No token
+			$this->session_machine = $this->smalldb->ref($this->session_machine_null_ref);
+		}
+
+		// Update token on state change
+		$t = $this;
+		$this->session_machine->after_transition_cb[] = function($ref, $transition_name, $arguments, $return_value, $returns) use ($t) {
+			if (!$ref->state) {
+				// Remove cookie when session is terminated
+				setcookie($t->cookie_name, null, time() + $t->cookie_ttl, '/', null, !empty($_SERVER['HTTPS']), true);
+				if (isset($_SESSION)) {
+					// Kill PHP session if in use
+					session_regenerate_id(true);
+				}
+			} else {
+				// Update cookie on state change
+				setcookie($t->cookie_name, $ref->id, time() + $t->cookie_ttl, '/', null, !empty($_SERVER['HTTPS']), true);
+			}
+		};
+
+		// Everything else is up to the state machine
+	}
+
+
+	/**
+	 * Get session machine which manages all stuff around login and session.
+	 */
+	public function getSessionMachine()
+	{
+		if ($this->session_machine === null) {
+			throw new RuntimeException('Session machine not ready ‒ '.__METHOD__.' called too soon.');
+		}
+		return $this->session_machine;
+	}
+
+
+	/// @copydoc Smalldb\StateMachine\Auth\IAuth::getUserId()
+	public function getUserId()
+	{
+		if ($this->session_machine === null) {
+			throw new RuntimeException('Session machine not ready ‒ '.__METHOD__.' called too soon.');
+		}
+		return $this->session_machine->state !== '' ? $this->session_machine[$this->user_id_property] : null;
+	}
+
+
+	/// @copydoc Smalldb\StateMachine\Auth\IAuth::hasUserRoles()
+	public function hasUserRoles($roles)
+	{
+		if ($this->session_machine === null) {
+			throw new RuntimeException('Session machine not ready ‒ '.__METHOD__.' called too soon.');
+		}
+
+		if (is_array($roles)) {
+			return count(array_intersect($this->getUserRoles(), $roles)) > 0;
+		} else {
+			return in_array($roles, $this->getUserRoles());
+		}
+	}
+
+	/// @copydoc Smalldb\StateMachine\Auth\IAuth::isAllMighty()
+	public function isAllMighty()
+	{
+		return ($this->all_mighty_cli && empty($_SERVER['REMOTE_ADDR']) && php_sapi_name() == 'cli')
+			|| ($this->all_mighty_user_role && $this->hasUserRoles($this->all_mighty_user_role));
+	}
+
+
+	/**
+	 * Get list of user's roles, or null if not logged in.
+	 *
+	 * @note User's roles are typically not property of the session
+	 * 	machine, but they can be calculated property of the machine.
+	 * 	Therefore, there is no need to complicate this API with users.
+	 */
+	protected function getUserRoles()
+	{
+		if ($this->session_machine === null) {
+			throw new RuntimeException('Session machine not ready ‒ '.__METHOD__.' called too soon.');
+		}
+
+		if ($this->session_machine->state == '') {
+			return null;
+		} else {
+			return (array) $this->session_machine[$this->user_roles_property];
+		}
+	}
+
+}
+
---
+++ class/Auth/SharedTokenMachine.php
@@ -0,0 +1,210 @@
+<?php
+/*
+ * Copyright (c) 2016, Josef Kufner  <josef@kufner.cz>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+namespace Smalldb\StateMachine\Auth;
+
+/**
+ * Session management using a shared token. Works well with CookieAuth
+ * authenticator.
+ */
+class SharedTokenMachine extends \Smalldb\StateMachine\FlupdoCrudMachine
+{
+	/**
+	 * Setup session machine
+	 */
+	protected function setupDefaultMachine($config)
+	{
+
+		// State function
+		if ($this->state_select === null) {
+			$this->state_select = '"Authenticated"'; // TODO
+		}
+
+		// Exists state only
+		$this->states = [
+			'Authenticated' => [
+				'description' => _('User is authenticated and active.'),
+				'color' => '#ddffcc',
+			],
+			'Prepared' => [
+				'description' => _('Session token is waiting for user (i.e. to click login link sent via e-mail).'),
+				'color' => '#ffffcc',
+			],
+			'Expired' => [
+				'description' => _('Session has expired. Awaiting clean up.'),
+				'color' => '#ffddcc',
+			],
+		];
+
+		$this->access_policies = [
+			'admin' => [
+				'type' => 'nobody',
+				'arrow_tail' => 'nonetee',
+			],
+			'owner' => [
+				'type' => 'owner',
+				'owner_property' => 'user_id',
+				'arrow_tail' => 'dot',
+			],
+			'anonymous' => [
+				'type' => 'anonymous',
+				'arrow_tail' => 'odot',
+			],
+		];
+
+		// Actions
+		$this->actions = [
+			'login' => [
+				'description' => _('Login user using password.'),
+				'color' => '#44aa00',
+				'returns' => self::RETURNS_NEW_ID,
+				'transitions' => [
+					'' => [
+						'targets' => [ '', 'Authenticated' ],
+						'access_policy' => 'anonymous',
+					],
+				],
+			],
+			'useToken' => [
+				'description' => _('Login user using prepared token'),
+				'transitions' => [
+					'Prepared' => [
+						'targets' => [ 'Authenticated' ],
+						'access_policy' => 'anonymous',
+					],
+				],
+			],
+			'logout' => [
+				'description' => _('Logout user.'),
+				'color' => '#aa4400',
+				'transitions' => [
+					'Authenticated' => [
+						'targets' => [ '' ],
+						'access_policy' => 'owner',
+					],
+					'Prepared' => [
+						'targets' => [ '' ],
+						'access_policy' => 'admin',
+					],
+					'Expired' => [
+						'targets' => [ '' ],
+						'access_policy' => 'owner',
+					],
+				],
+			],
+			'checkSession' => [
+				'description' => _('Check whether the session is valid.'),
+				'color' => '#aa88ff',
+				'transitions' => [
+					'' => [
+						'targets' => [ '', 'Authenticated' ],
+						'access_policy' => 'anonymous',
+					],
+				],
+			],
+			'prepare' => [
+				'description' => _('Prepare session token for later use.'),
+				'returns' => self::RETURNS_NEW_ID,
+				'transitions' => [
+					'' => [
+						'targets' => [ 'Prepared' ],
+					],
+				],
+			],
+			'expire' => [
+				'description' => _('Expire session. Timed transition.'),
+				'color' => '#aaaaaa',
+				'transitions' => [
+					'Authenticated' => [
+						'targets' => [ 'Expired' ],
+					],
+					'Prepared' => [
+						'targets' => [ 'Expired' ],
+					],
+				],
+			],
+		];
+
+		// Merge with config
+		if (isset($config['actions'])) {
+			$this->actions = array_replace_recursive($this->actions, $config['actions']);
+		}
+		if (isset($config['states'])) {
+			$this->states = array_replace_recursive($this->states, $config['states']);
+		}
+		if (isset($config['access_policies'])) {
+			$this->access_policies = array_replace_recursive($this->access_policies, $config['access_policies']);
+		}
+	}
+
+
+	/**
+	 * Login user
+	 *
+	 * @see Timing attack prevention:
+	 * 	http://blog.ircmaxell.com/2014/11/its-all-about-time.html
+	 */
+	protected function login($ref, $user, $password)
+	{
+		// Get user
+		$user_list = $this->backend->createListing(['type' => 'user', 'email' => $user])->fetchAll();
+		if (empty($user_list)) {
+			$user_ref = null;
+		} else {
+			$user_ref = reset($user_list);
+		}
+
+		// Check password
+		$dummy_hash = password_hash((string) time(), PASSWORD_DEFAULT); // prevent timing attack to $user
+		$passwd_hash = $user_ref ? $user_ref['password'] : $dummy_hash;
+		$passwd_valid = $user_ref && password_verify($password, $passwd_hash);
+
+		if (!$passwd_valid) {
+			// One more protection against timing attack
+			time_nanosleep(0, abs(crc32($user.$password.time().__FILE__) % 200000));
+			return false;
+		}
+
+		// Login OK - generate session token
+		$session_id = base64_encode(openssl_random_pseudo_bytes(12));
+		$session_token = base64_encode(openssl_random_pseudo_bytes(24));
+
+		// Store session token
+		$n = $this->flupdo->insert()->into($this->flupdo->quoteIdent($this->table))
+			->insert('id, token, user_id')
+			->values([[$session_id, password_hash($session_token, PASSWORD_DEFAULT), $user_ref->id]])
+			->exec();
+
+		if ($n == 1) {
+			return $session_id;
+		} else {
+			return false;
+		}
+	}
+
+
+	/**
+	 * Logout user - simply destroy session.
+	 */
+	protected function logout($ref)
+	{
+		return parent::delete($ref);
+	}
+
+}
+
