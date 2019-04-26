b5ab165907fc00602139da11f9c40db4f94959e8
Josef Kufner <josef@kufner.cz>
2019-04-26 18:50:28 +0200

    Utils: Fix warnings, remove obsolete code

--- class/Utils/Utils.php
+++ class/Utils/Utils.php
@@ -83,8 +83,12 @@ class Utils
 
 		$status = 0;		// Current status of parser
 		$append = 0;		// Append value to result after token is processed ?
+		$key = null;
+		$fmt = null;
 		$result = array();
+		/** @var callable|null $process_function */
 		$process_function = null;
+		/** @var callable|null $format_function */
 		$format_function = null;
 
 		$raw_values = array_slice(func_get_args(), 3);
@@ -178,6 +182,7 @@ class Utils
 			if ($append) {
 				$append = false;
 				$raw = null;
+				$v = null;
 
 				// get value
 				foreach ($raw_values as $rv) {
@@ -317,8 +322,8 @@ class Utils
 	/**
 	 * Decode JSON string.
 	 *
-	 * @param $json_str String to parse.
-	 * @param $filename If set the exception message will contain $filename.
+	 * @param string $json_str String to parse.
+	 * @param string $filename If set the exception message will contain $filename.
 	 */
 	static function parse_json_string($json_str, $filename = null)
 	{
@@ -331,34 +336,4 @@ class Utils
 	}
 
 }
-
-
-if (!function_exists('json_last_error_msg')) {
-/**
- * json_last_error_msg() implementation for PHP < 5.5
- */
-function json_last_error_msg()
-{
-	switch (json_last_error()) {
-		default:
-			// Other errors are only in PHP >=5.5, where native 
-			// implementation of this function is used.
-			return null;
-		case JSON_ERROR_DEPTH:
-			$msg = 'Maximum stack depth exceeded';
-			break;
-		case JSON_ERROR_STATE_MISMATCH:
-			$msg = 'Underflow or the modes mismatch';
-			break;
-		case JSON_ERROR_CTRL_CHAR:
-			$msg = 'Unexpected control character found';
-			break;
-		case JSON_ERROR_SYNTAX:
-			$msg = 'Syntax error, malformed JSON';
-			break;
-		case JSON_ERROR_UTF8:
-			$msg = 'Malformed UTF-8 characters, possibly incorrectly encoded';
-			break;
-	}
-}}
 
